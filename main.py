9994c07933ba8b4f370933ad92c7b454d02184b3
Xanops <pzvpetr@gmail.com>
2021-03-16 19:04:44 +0300

    second release

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('page.html', title=title)
+
+
 @app.route('/distribution')
-def index():
+def loop():
     sp = ['Ридли Скотт', "Энди Уир", "Марк Уотни", "Венката Капур", "Тедди Сандерс", "Шон Бин"]
-    return render_template('index.html', sp=sp)
+    return render_template('loop.html', sp=sp)
 
 
 if __name__ == '__main__':
