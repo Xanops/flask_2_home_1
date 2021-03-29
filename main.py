f0f790d029c4e60225ef3e0fb3e788d5d00a2d47
Xanos <74895133+Xanops@users.noreply.github.com>
2021-03-29 23:28:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 @app.route('/<title>')
 @app.route('/index/<title>')
 def index(title):
-    return render_template('page.html', title=title)
+    return render_template('base.html', title=title)
 
 
 @app.route('/distribution')
